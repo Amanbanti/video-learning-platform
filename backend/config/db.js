009f8b0e28d6e156a85e7f1c71343039f9a2aac1
Amanbanti <amanbanti2011@gmail.com>
2025-09-21 18:31:15 +0300

    db config

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 30000, // 30 seconds timeout for server selection
+      socketTimeoutMS: 60000, // 60 seconds timeout for socket connection
+    });
+    console.log("MongoDB Connected: " + conn.connection.host);
+  } catch (error) {
+    console.error("Error connecting to MongoDB: ", error.message);
+    process.exit(1); // Exit the process with failure
+  }
+};
