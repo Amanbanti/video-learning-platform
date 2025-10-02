42cdce10629828d99f75e8f8c6b37c747ff47a50
Amanbanti <amanbanti2011@gmail.com>
2025-10-02 18:18:08 +0300

    useRoute commit

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { registerUser, loginUser, uploadReceipt ,verifyOtp } from "../controllers/userController.js";
+import { registerUser, loginUser, uploadReceipt ,verifyOtp,logoutUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/verify-otp", verifyOtp);
 router.post("/login", loginUser);
+router.post("/logout", logoutUser)
 router.put("/:id/receipt", uploadReceipt);
 
 export default router;
