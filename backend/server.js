46b0183f4eab262031b97627d60caa5ea99e6e45
Amanbanti <amanbanti2011@gmail.com>
2025-10-25 13:48:14 +0300

    firxing the error

--- backend/server.js
+++ backend/server.js
@@ -48,7 +48,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
   // Catch-all route for frontend
-  app.get('/*', (req, res) => {
+  app.get('/:path(.*)', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
   });
 } else {
