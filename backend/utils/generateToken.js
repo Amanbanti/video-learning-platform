7a27f2b82637e979d2073e83ec6b3617691fbd34
Amanbanti <amanbanti2011@gmail.com>
2025-10-25 13:33:29 +0300

    fix error

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -11,7 +11,7 @@ export const generateToken = (res, user) => {
   res.cookie('jwt', token, {
     httpOnly: true,
     secure: true, // must be true in prod
-    sameSite: 'None', // cross-site cookies
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'Lax', // cross-site cookies
     maxAge: 1 * 24 * 60 * 60 * 1000, // 1 day
   });
 };
